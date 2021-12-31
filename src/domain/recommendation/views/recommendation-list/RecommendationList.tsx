e66bc54e546be3d1237cf811a3279adbfaeba5fa
Ourai Lin <ourairyu@gmail.com>
2021-12-31 10:56:25 +0800

    feat: FinTech recommendation list view

--- src/domain/recommendation/views/recommendation-list/RecommendationList.tsx
+++ src/domain/recommendation/views/recommendation-list/RecommendationList.tsx
@@ -1,15 +1,42 @@
 import { ReactNode } from 'react';
+import { Link } from 'umi';
 
 import { ListViewHeadlessWidget } from '@/shared/components/widget/headless';
+
+import style from './style.scss';
 
 export default class RecommendationListViewWidget extends ListViewHeadlessWidget {
   public render(): ReactNode {
     return (
-      <ul>
-        {['培训推荐', '咨询推荐', '助手推荐'].map((text) => (
-          <li key={text}>{text}</li>
-        ))}
-      </ul>
+      <div className={style.RecommendationListViewWidget}>
+        <h2 className={style['RecommendationListViewWidget-heading']}>
+          推荐列表
+        </h2>
+        <ul className={style['RecommendationListViewWidget-list']}>
+          {['培训推荐', '咨询推荐', '助手推荐', '招商推荐'].map((text) => (
+            <li
+              className={style['RecommendationListViewWidget-item']}
+              key={text}
+            >
+              <Link
+                className={style['RecommendationListViewWidget-itemContent']}
+                to="/fintech/recommendation/specific/biz-sides"
+              >
+                <span
+                  className={style['RecommendationListViewWidget-itemIcon']}
+                >
+                  图
+                </span>
+                <span
+                  className={style['RecommendationListViewWidget-itemText']}
+                >
+                  {text}
+                </span>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
     );
   }
 }
